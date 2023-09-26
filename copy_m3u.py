a3b43ed6d69173eed9c3d9cabf9ae80b8fe5f5f0
boleechat <133635270+boleechat@users.noreply.github.com>
2023-09-26 16:29:34 +0800

    Update copy_m3u.py

--- copy_m3u.py
+++ copy_m3u.py
@@ -1,41 +1,37 @@
 import os
 import requests
-from base64 import b64encode
 
 def main():
     source_url = 'https://ghproxy.com/https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u'
-    destination_url = 'https://api.github.com/repos/boleechat/live/contents/globe.m3u'
-    access_token = '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'
-
-    # Send GET request to retrieve the original m3u file content
+    destination_url = 'https://github.com/boleechat/live/globe.m3u'
+    
+    # 发送GET请求获取原始m3u文件内容
     response = requests.get(source_url)
     content = response.text
-
-    # Modify the content as needed
-    # ...
-
-    # Encode the modified content as base64
-    encoded_content = b64encode(content.encode()).decode()
-
-    # Prepare the request payload
-    payload = {
-        "message": "Update globe.m3u",
-        "content": encoded_content
-    }
-
-    # Set the Authorization header with the access token
+    
+    # 替换第一行为#EXTM3U
+    content = content.replace(content.split('\n')[0], '#EXTM3U')
+    
+    # 将包含"https://cntv.sbs/tv?auth"的内容替换为"https://www.szqcom.repl.co/PLTV/tivihk.php?url=https://cntv.sbs/tv?auth"
+    content = content.replace('https://cntv.sbs/tv?auth', 'https://www.szqcom.repl.co/PLTV/tivihk.php?url=https://cntv.sbs/tv?auth')
+    
+    # 将修改后的内容写入临时文件
+    with open('globe.m3u', 'w') as file:
+        file.write(content)
+    
+    # 提交临时文件到GitHub
     headers = {
-        'Authorization': f'Token {access_token}',
-        'Content-Type': 'application/json'
+        'Authorization': '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',
     }
-
-    # Send PUT request to create or update the file on GitHub
-    response = requests.put(destination_url, json=payload, headers=headers)
-
+    files = {
+        'file': ('globe.m3u', open('globe.m3u', 'rb')),
+    }
+    response = requests.put(destination_url, headers=headers, files=files)
+    
     if response.status_code == 200:
-        print('m3u file successfully updated!')
+        print('m3u文件成功复制！')
     else:
-        print('Failed to update m3u file.')
+        print('无法复制m3u文件。')
 
 if __name__ == '__main__':
     main()
