7b34c222c884aab88099df4b6b68599cfb05ca24
boleechat <133635270+boleechat@users.noreply.github.com>
2023-09-27 14:23:35 +0800

    Update copy_m3u.py

--- copy_m3u.py
+++ copy_m3u.py
@@ -1,46 +1,31 @@
-import os
 import requests
+import base64
 
 def main():
-    source_url = 'https://ghproxy.com/https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u'
-    destination_url = 'https://raw.githubusercontent.com/boleechat/live/main/globe.m3u'
-    
-    # 发送GET请求获取原始m3u文件内容
-    response = requests.get(source_url)
-    content = response.text
-    
-    # 替换第一行为#EXTM3U
-    content = content.replace(content.split('\n')[0], '#EXTM3U')
-    
-    # 将包含"https://cntv.sbs/tv?auth"的内容替换为"https://www.szqcom.repl.co/PLTV/tivihk.php?url=https://cntv.sbs/tv?auth"
-    content = content.replace('https://cntv.sbs/tv?auth', 'https://www.szqcom.repl.co/PLTV/tivihk.php?url=https://cntv.sbs/tv?auth')
 
-    
-    # 替换操作
-    # ...
+  source_url = 'https://ghproxy.com/https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u'
+  destination_url = 'https://raw.githubusercontent.com/boleechat/live/main/globe.m3u'
 
-       # 将修改后的内容写入临时文件
-    #with open('globe.m3u', 'w') as file:
-        #file.write(content)
-    
-    # 提交临时文件到GitHub
-    headers = {
-        'Authorization': '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',
-    }
-    
-    # 读取文本内容直接使用
-    content = response.text
+  response = requests.get(source_url)
+  content = response.text
 
-    # 无需再做额外编码
-    headers = {'Content-Type': 'text/plain; charset=utf-8'}
+  # 替换内容
 
-    # 直接作为字符串提交
-    response = requests.put(destination_url, data=content, headers=headers)
-    
-    if response.status_code == 200:
-      print('更新成功')
-    else:
-      print('更新失败')
+  content = content.replace(content.split('\n')[0], '#EXTM3U')
+  content = content.replace('https://cntv.sbs/tv?auth', 'https://www.szqcom.repl.co/PLTV/tivihk.php?url=https://cntv.sbs/tv?auth')
+
+  # base64编码content
+  content = base64.b64encode(content.encode('utf-8'))
+
+  headers = {'Content-Type': 'text/plain; charset=utf-8',
+             'Authorization': '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'}
+
+  response = requests.put(destination_url, data=content, headers=headers)
+
+  if response.status_code == 200:
+    print('更新成功')
+  else:
+    print('更新失败')
 
 if __name__ == '__main__':
-    main()
+  main()
