da4d60d293d06ad54fe442795adeb41171643a58
boleechat <133635270+boleechat@users.noreply.github.com>
2023-09-27 13:50:11 +0800

    Update copy_m3u.py

--- copy_m3u.py
+++ copy_m3u.py
@@ -3,7 +3,7 @@ import requests
 
 def main():
     source_url = 'https://ghproxy.com/https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/global.m3u'
-    destination_url = 'https://github.com/boleechat/live/globe.m3u'
+    destination_url = 'https://raw.githubusercontent.com/boleechat/live/main/globe.m3u'
     
     # 发送GET请求获取原始m3u文件内容
     response = requests.get(source_url)
@@ -23,22 +23,22 @@ def main():
     # ...
 
        # 将修改后的内容写入临时文件
-    with open('globe.m3u', 'w') as file:
-        file.write(content)
+    #with open('globe.m3u', 'w') as file:
+        #file.write(content)
     
     # 提交临时文件到GitHub
     headers = {
         'Authorization': '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',
     }
-    files = {
-        'file': ('globe.m3u', open('globe.m3u', 'rb')),
-    }
-    response = requests.put(destination_url, headers=headers, files=files)
+    #files = {
+        #'file': ('globe.m3u', open('globe.m3u', 'rb')),
+    #}
+   response = requests.put(destination_url, data=content, headers=headers)
     
     if response.status_code == 200:
-        print('m3u文件成功复制！')
+      print('更新成功')
     else:
-        print('无法复制m3u文件。')
+      print('更新失败')
 
 if __name__ == '__main__':
     main()
