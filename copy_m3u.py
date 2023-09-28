3d25ab14cf7e3df72694f71dc5c306b60cbf816c
boleechat <133635270+boleechat@users.noreply.github.com>
2023-09-28 14:57:52 +0800

    Update copy_m3u.py

--- copy_m3u.py
+++ copy_m3u.py
@@ -20,18 +20,29 @@ def main():
     'Authorization': f'token {token}' 
   }
 
+  # 获取文件信息
+  response = requests.get(api_url, headers=headers)
+  file_info = response.json()
+
+  # 保存文件的SHA
+  file_sha = file_info['sha']
+
   data = {
     "message": "update globe.m3u",
-    "content": content
+    "content": content,
+    "sha": file_sha  # 使用文件的SHA
   }
 
   response = requests.put(api_url, json=data, headers=headers)
-  print('Status code:', response.status_code)
-  print('Response:', response.json())
+
   if response.status_code == 201:
     print('更新成功')
+    print('Status code:', response.status_code)
+    print('Response:', response.json())
   else:
     print('更新失败')
+    print('Status code:', response.status_code)
+    print('Response:', response.json())
 
 if __name__ == '__main__':
   main()
